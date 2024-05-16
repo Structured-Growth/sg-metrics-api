59bae85a7ca84354867a4766ec5175e26aa33b80
ryzhov <9920529@gmail.com>
2024-05-16 12:26:30 +0300

    fix(SG-187): migrations

--- database/migrations/20240409140555-metric-type.js
+++ database/migrations/20240409140555-metric-type.js
@@ -1,65 +1,67 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+const Sequelize = require("sequelize");
+
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("metric_types", {
-      id: {
-        type: Sequelize.SMALLINT,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      org_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      account_id: {
-        type: Sequelize.INTEGER,
-      },
-      region: {
-        type: Sequelize.STRING(10),
-        allowNull: false,
-      },
-      metric_category_id: {
-        type: Sequelize.SMALLINT,
-        references: {
-          model: "metric_categories",
-          key: "id",
-        },
-        onDelete: "RESTRICT",
-      },
-      title: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-      },
-      code: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-        unique: true,
-      },
-      unit: {
-        type: Sequelize.STRING(25),
-      },
-      factor: {
-        type: Sequelize.SMALLINT,
-      },
-      related_to: {
-        type: Sequelize.STRING(50),
-      },
-      version: {
-        type: Sequelize.SMALLINT,
-      },
-      status: {
-        type: Sequelize.STRING(15),
-        allowNull: false,
-      },
-      created_at: Sequelize.DATE,
-      updated_at: Sequelize.DATE,
-      deleted_at: Sequelize.DATE,
-    });
-  },
+	async up(queryInterface) {
+		await queryInterface.createTable("metric_types", {
+			id: {
+				type: Sequelize.SMALLINT,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			org_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+			},
+			account_id: {
+				type: Sequelize.INTEGER,
+			},
+			region: {
+				type: Sequelize.STRING(10),
+				allowNull: false,
+			},
+			metric_category_id: {
+				type: Sequelize.SMALLINT,
+				references: {
+					model: "metric_categories",
+					key: "id",
+				},
+				onDelete: "RESTRICT",
+			},
+			title: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+			},
+			code: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+				unique: true,
+			},
+			unit: {
+				type: Sequelize.STRING(25),
+			},
+			factor: {
+				type: Sequelize.SMALLINT,
+			},
+			related_to: {
+				type: Sequelize.STRING(50),
+			},
+			version: {
+				type: Sequelize.SMALLINT,
+			},
+			status: {
+				type: Sequelize.STRING(15),
+				allowNull: false,
+			},
+			created_at: Sequelize.DATE,
+			updated_at: Sequelize.DATE,
+			deleted_at: Sequelize.DATE,
+		});
+	},
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("metric_types");
-  },
+	async down(queryInterface) {
+		await queryInterface.dropTable("metric_types");
+	},
 };
--- database/migrations/20240409140622-metric-category-metadata.js
+++ database/migrations/20240409140622-metric-category-metadata.js
@@ -1,46 +1,48 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+const Sequelize = require("sequelize");
+
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("metric_categories_metadata", {
-      id: {
-        type: Sequelize.SMALLINT,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      org_id: {
-        type: Sequelize.INTEGER,
-      },
-      account_id: {
-        type: Sequelize.INTEGER,
-      },
-      region: {
-        type: Sequelize.STRING(10),
-        allowNull: false,
-      },
-      metric_category_id: {
-        type: Sequelize.SMALLINT,
-        references: {
-          model: "metric_categories",
-          key: "id",
-        },
-        onDelete: "CASCADE",
-      },
-      name: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-      },
-      value: {
-        type: Sequelize.STRING(255),
-      },
-      created_at: Sequelize.DATE,
-      updated_at: Sequelize.DATE,
-      deleted_at: Sequelize.DATE,
-    });
-  },
+	async up(queryInterface) {
+		await queryInterface.createTable("metric_categories_metadata", {
+			id: {
+				type: Sequelize.SMALLINT,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			org_id: {
+				type: Sequelize.INTEGER,
+			},
+			account_id: {
+				type: Sequelize.INTEGER,
+			},
+			region: {
+				type: Sequelize.STRING(10),
+				allowNull: false,
+			},
+			metric_category_id: {
+				type: Sequelize.SMALLINT,
+				references: {
+					model: "metric_categories",
+					key: "id",
+				},
+				onDelete: "CASCADE",
+			},
+			name: {
+				type: Sequelize.STRING(100),
+				allowNull: false,
+			},
+			value: {
+				type: Sequelize.STRING(255),
+			},
+			created_at: Sequelize.DATE,
+			updated_at: Sequelize.DATE,
+			deleted_at: Sequelize.DATE,
+		});
+	},
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("metric_categories_metadata");
-  },
+	async down(queryInterface) {
+		await queryInterface.dropTable("metric_categories_metadata");
+	},
 };
