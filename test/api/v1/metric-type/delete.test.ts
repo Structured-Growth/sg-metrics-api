ded89f0ccb8d1e6afb3e3996263b4140ee9fc94f
anton-devline <anton.z@devline.pro>
2024-06-10 13:34:41 +0100

    chore(SG-212): metrics tests

--- test/api/v1/metric-type/delete.test.ts
+++ test/api/v1/metric-type/delete.test.ts
@@ -1,9 +1,9 @@
 import "../../../../src/app/providers";
 import { assert } from "chai";
 import { initTest } from "../../../common/init-test";
-import {RegionEnum} from "@structured-growth/microservice-sdk";
-import {container} from "@structured-growth/microservice-sdk";
-import {App} from "../../../../src/app/app";
+import { RegionEnum } from "@structured-growth/microservice-sdk";
+import { container } from "@structured-growth/microservice-sdk";
+import { App } from "../../../../src/app/app";
 
 describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 	const { server, context } = initTest();
@@ -36,7 +36,6 @@ describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 		assert.equal(statusCode, 201);
 		assert.isNumber(body.id);
 		context.createdMetricCategoryId = body.id;
-
 	});
 
 	it("Should create metric type", async () => {
@@ -67,7 +66,7 @@ describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 			region: RegionEnum.US,
 			metricCategoryId: context["createdMetricCategoryId"],
 			title: code,
-			code: code + '2',
+			code: code + "2",
 			unit: code,
 			factor: factor,
 			relatedTo: code,
@@ -87,6 +86,7 @@ describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 		const { statusCode, body } = await server.post("/v1/metrics").send([
 			{
 				orgId: orgId,
+				region: RegionEnum.US,
 				accountId: accountId,
 				userId: userId,
 				metricCategoryId: context.createdMetricCategoryId,
@@ -97,25 +97,25 @@ describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 				value: value,
 				takenAt: "2024-05-16T14:30:00+00:00",
 				takenAtOffset: takenAtOffset,
-			}
+			},
 		]);
 		assert.equal(statusCode, 201);
 	}).timeout(1800000);
 
 	it("Should return metric type", async () => {
 		const { statusCode, body } = await server.get(`/v1/metric-type/${context.createdMetricTypeId}`).send({
-			MetricTypeId: context["createdMetricTypeId"]
+			MetricTypeId: context["createdMetricTypeId"],
 		});
 		assert.equal(statusCode, 200);
 		assert.isNumber(body.id);
 	});
 
-	it("Should return error because  metric type is not empty", async () => {
+	it("Should return error because metric type is not empty", async function () {
 		const { statusCode, body } = await server.delete(`/v1/metric-type/${context.createdMetricType2Id}`);
 		assert.equal(statusCode, 422);
 		assert.isDefined(body.validation);
 		assert.isString(body.message);
-	});
+	}).timeout(10000);
 
 	it("Should delete metric type", async () => {
 		const { statusCode, body } = await server.delete(`/v1/metric-type/${context.createdMetricTypeId}`);
@@ -124,7 +124,7 @@ describe("DELETE /api/v1/metric-type/:metricTypeId", () => {
 
 	it("Should return error because metric type was deleted", async () => {
 		const { statusCode, body } = await server.get(`/v1/metric-type/${context.createdMetricTypeId}`).send({
-			MetricTypeId: context["createdMetricTypeId"]
+			MetricTypeId: context["createdMetricTypeId"],
 		});
 		assert.equal(statusCode, 404);
 		assert.isString(body.message);
