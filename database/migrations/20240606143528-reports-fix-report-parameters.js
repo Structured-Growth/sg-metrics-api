83eb6e387d9e5c4a83ab99df003e0bd27a150772
ryzhov <9920529@gmail.com>
2024-06-10 16:12:21 +0300

    fix(SG-187): migration

--- database/migrations/20240606143528-reports-fix-report-parameters.js
+++ database/migrations/20240606143528-reports-fix-report-parameters.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict";
 
-/** @type {import('sequelize-cli').Migration} */
+const Sequelize = require("sequelize");
+
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('reports', 'report_parameters', {
-      type: Sequelize.TEXT,
-      allowNull: false,
-    });
-  },
+	async up(queryInterface) {
+		await queryInterface.changeColumn("reports", "report_parameters", {
+			type: Sequelize.TEXT,
+			allowNull: false,
+		});
+	},
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn('reports', 'report_parameters', {
-      type: Sequelize.STRING(500),
-      allowNull: false,
-    });
-  }
+	async down(queryInterface) {
+		await queryInterface.changeColumn("reports", "report_parameters", {
+			type: Sequelize.STRING(500),
+			allowNull: false,
+		});
+	},
 };
