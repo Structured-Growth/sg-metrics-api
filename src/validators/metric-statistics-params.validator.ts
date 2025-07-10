0aab8837340e0bbdde823ebed12acce4837d1517
anton-devline <anton.z@devline.pro>
2025-07-10 19:17:00 +0100

    feat(SG-599): Extend default policies in Policies API

--- src/validators/metric-statistics-params.validator.ts
+++ src/validators/metric-statistics-params.validator.ts
@@ -5,8 +5,8 @@ const isoFormatWithTimezone = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-
 export const MetricStatisticsParamsValidator = joi.object({
 	query: joi.object(),
 	body: joi.object({
-		accountId: joi.number().positive().label("validator.metrics.accountId"),
-		userId: joi.number().positive().label("validator.metrics.userId"),
+		accountId: joi.number().positive().required().label("validator.metrics.accountId"),
+		userId: joi.number().positive().required().label("validator.metrics.userId"),
 		startPreviousPeriod: joi
 			.string()
 			.regex(isoFormatWithTimezone)
@@ -17,7 +17,7 @@ export const MetricStatisticsParamsValidator = joi.object({
 			.regex(isoFormatWithTimezone)
 			.required()
 			.label("validator.metrics.startCurrentPeriod"),
-		lowThreshold: joi.number().positive().label("validator.metrics.lowThreshold"),
-		highThreshold: joi.number().positive().label("validator.metrics.highThreshold"),
+		lowThreshold: joi.number().positive().required().label("validator.metrics.lowThreshold"),
+		highThreshold: joi.number().positive().required().label("validator.metrics.highThreshold"),
 	}),
 });
